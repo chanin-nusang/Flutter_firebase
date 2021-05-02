28f035e88e8e23d7d27fcb6d1c24e2c6ec736efe
realchanin <6110110589@psu.ac.th>
2021-05-02 21:52:46 +0700

    form validate

--- lib/screen/form_screen.dart
+++ lib/screen/form_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/model/student.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class FormScreen extends StatefulWidget {
   @override
@@ -6,6 +8,9 @@ class FormScreen extends StatefulWidget {
 }
 
 class _FormScreenState extends State<FormScreen> {
+  final formkey = GlobalKey<FormState>();
+  Student myStudent = Student();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,48 +20,79 @@ class _FormScreenState extends State<FormScreen> {
       body: Container(
         padding: EdgeInsets.all(20),
         child: Form(
+            key: formkey,
             child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "ชื่อ",
-                style: TextStyle(fontSize: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "ชื่อ",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  TextFormField(
+                    validator: RequiredValidator(errorText: "กรุณาป้อนชื่อ"),
+                    onSaved: (String fname) {
+                      myStudent.fname = fname;
+                    },
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    "นามสกุล",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  TextFormField(
+                    validator: RequiredValidator(errorText: "กรุณาป้อนนามสกุล"),
+                    onSaved: (String lname) {
+                      myStudent.lname = lname;
+                    },
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    "อีเมล",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  TextFormField(
+                    validator: MultiValidator([
+                      EmailValidator(errorText: "รูปแบบอีเมลไม่ถูกต้อง"),
+                      RequiredValidator(errorText: "กรุณาป้อนอีเมล")
+                    ]),
+                    onSaved: (String email) {
+                      myStudent.email = email;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    "คะแนน",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  TextFormField(
+                    validator: RequiredValidator(errorText: "กรุณาป้อนคะแนน"),
+                    onSaved: (String score) {
+                      myStudent.score = score;
+                    },
+                    keyboardType: TextInputType.number,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        child: Text("บันทึกข้อมูล",
+                            style: TextStyle(fontSize: 20)),
+                        onPressed: () {
+                          if (formkey.currentState.validate()) {
+                            formkey.currentState.save();
+                          }
+                        }),
+                  ),
+                ],
               ),
-              TextFormField(),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                "นามสกุล",
-                style: TextStyle(fontSize: 20),
-              ),
-              TextFormField(),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                "อีเมล",
-                style: TextStyle(fontSize: 20),
-              ),
-              TextFormField(),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                "คะแนน",
-                style: TextStyle(fontSize: 20),
-              ),
-              TextFormField(),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                    child: Text("บันทึกข้อมูล", style: TextStyle(fontSize: 20)),
-                    onPressed: () {}),
-              ),
-            ],
-          ),
-        )),
+            )),
       ),
     );
   }
