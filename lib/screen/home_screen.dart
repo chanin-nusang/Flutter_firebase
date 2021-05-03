be018ddb27ce780bbdccd282e4efcdd73d05f8bb
realchanin <6110110589@psu.ac.th>
2021-05-04 02:33:19 +0700

    connect register screen with Firebase Auth

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -13,47 +13,49 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(10, 150, 10, 150),
-        child: Column(
-          children: [
-            new SvgPicture.asset(
-              assetName,
-              height: 200,
-              width: 200,
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return RegisterScreen();
-                    }));
-                  },
-                  icon: Icon(Icons.add),
-                  label: Text(
-                    "สร้างบัญชีผู้ใช้",
-                    style: TextStyle(fontSize: 20),
-                  )),
-            ),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) {
-                      return LoginScreen();
-                    }));
-                  },
-                  icon: Icon(Icons.login),
-                  label: Text(
-                    "เข้าสู่ระบบ",
-                    style: TextStyle(fontSize: 20),
-                  )),
-            )
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              new SvgPicture.asset(
+                assetName,
+                height: 200,
+                width: 200,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return RegisterScreen();
+                      }));
+                    },
+                    icon: Icon(Icons.add),
+                    label: Text(
+                      "สร้างบัญชีผู้ใช้",
+                      style: TextStyle(fontSize: 20),
+                    )),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton.icon(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return LoginScreen();
+                      }));
+                    },
+                    icon: Icon(Icons.login),
+                    label: Text(
+                      "เข้าสู่ระบบ",
+                      style: TextStyle(fontSize: 20),
+                    )),
+              )
+            ],
+          ),
         ),
       ),
     );
